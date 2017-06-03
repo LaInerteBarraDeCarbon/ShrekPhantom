7016bc0823f5053d33463e2ad3c4e3d7d62401fb
Damian Alladio <damian.e.alladio@gmail.com>
2017-06-02 21:08:06 -0300

    Estructura básica

---
+++ src/shrek/Shrek.java
@@ -0,0 +1,5 @@
+package shrek;
+
+public class Shrek {
+
+}
---
+++ test/shrek/ShrekTest.java
@@ -0,0 +1,17 @@
+package shrek;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShrekTest {
+
+	private static String archivoIn = "Preparacion de Prueba/Lote de Prueba/Entrada/";
+	private static String archivoOut = "Ejecucion de Prueba/Salida Obtenida/";
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
